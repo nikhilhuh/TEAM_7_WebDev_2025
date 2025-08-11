34e9fe391a031f0b7ba6926b1578c22172f668c1
Nikhil Tiwari <nikhilhuh@gmail.com>
2025-08-11 13:55:11 +0530

    hero complete and gallery started

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     once: true,
     duration: 800,
   });
+
+  document.getElementById("curr-year").textContent = new Date().getFullYear();
+
+  const scrollDown = document.querySelector(".scroll-down");
+  if (window.screenY > 100) {
+    scrollDown.style.display = "hidden";
+  } else {
+    scrollDown.style.display = "";
+  }
 });
